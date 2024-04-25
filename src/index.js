dc2e05430f67216b76736ae25f6fa33eafd759ad
c209362 <jheel.rathod@iifl.com>
2024-04-25 18:59:15 +0530

    added cors and other minor changes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import express from 'express';
 import { randomUUID } from 'node:crypto';
 import {createServer} from 'node:http';
 import {Server} from 'socket.io';
+import cors from 'cors';
 
 const rooms = {};
 
 const app = express();
+app.use(cors());
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:5173"
+    }
+});
 
 io.on('connection',(socket)=> {
     console.log('a user connected');
@@ -17,11 +23,16 @@ io.on('connection',(socket)=> {
 
     socket.on('createGame',()=>{
         const roomId = randomUUID();
+        console.log(`Room created with ID: ${roomId}`);
         rooms[roomId] = {};
         socket.join(roomId);
         socket.emit("newGame",{roomId:roomId});
     })
 });
+
+app.get("/rooms",(req,res)=>{
+    res.send(rooms);    
+})
 
 server.listen(5000,()=> {
     console.log('Server is fire at http://localhost:5000');
